5b2d9143989bb96af1ba309dd46699ea3195eb27
Mustafa Said Helal <mustafasaidhelal@gmail.com>
2021-12-27 20:21:51 +0100

    First Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+type AppProps = {
+  message: React.ReactNode;
+}
+
+function App({message}: AppProps) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container mx-auto flex flex-col justify-center items-center h-screen'>
+      <h1 className="font-sans text-3xl text-center font-bold">{message}</h1>
+      <button className='rounded-md bg-[#1da1f2] text-white w-40 h-8 hover:bg-sky-700'>Increment</button>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './././'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App message={"Hello World"} />
   </React.StrictMode>,
   document.getElementById('root')
 );
